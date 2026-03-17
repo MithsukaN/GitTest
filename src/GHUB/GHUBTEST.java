597283f61fd53b87d9cb4934bfcbc9eccdeab166
IT25100027 <it25100027@my.sliit.lk>
2026-03-17 21:03:42 +0530

    print added

--- src/GHUB/GHUBTEST.java
+++ src/GHUB/GHUBTEST.java
@@ -4,7 +4,10 @@ public class GHUBTEST {
     public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello, World!"); // Prints the message to the console
+
+            System.out.println("Meka tm wenasa ");
         }
+
     }
 
 }
