a6e084bc787efd6b1a80898f49331433367b6965
IT25100027 <it25100027@my.sliit.lk>
2026-03-17 19:37:43 +0530

    store changes

---
+++ src/GHUB/GHUBTEST.java
@@ -0,0 +1,10 @@
+package GHUB;
+
+public class GHUBTEST {
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!"); // Prints the message to the console
+        }
+    }
+
+}
